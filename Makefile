836d78e3227b0c849803ce69c8dedc9a1b4d6b12
Michael Windels <mwindels@linux0.csc.uvic.ca>
2019-02-08 12:57:41 -0800

    Simplified vcheck1, added Makefile.

---
+++ Makefile
@@ -0,0 +1,6 @@
+vcheck1:
+	@python3 vcheck1.py
+
+.PHONY: clean
+clean:
+	@rm -r __pycache__
--- vcheck1.py
+++ vcheck1.py
@@ -1,28 +1,9 @@
-import sys
-import argparse
 import parse_tree as pt
 
-'''
-This function returns the formula input as a command line argument.
-
-Parameters
-----------
-args : list
-	The command line arguments, excluding the first one.
-
-Returns
--------
-formula : string
-	The boolean formula input to the command line.
-'''
-def get_formula(args):
-	parser = argparse.ArgumentParser(description='Check the validity of a boolean formula.')
-	parser.add_argument('formula', type=str, help='the boolean formula to check (in quotation marks if spaces and parentheses are involved)')
-	return parser.parse_args(args).formula
-
 if __name__ == '__main__':
+	print('Input a boolean formula:\n')
 	try:
-		if pt.ParseTree(get_formula(sys.argv[1:])).is_valid():
+		if pt.ParseTree(input()).is_valid():
 			print('Formula IS valid.')
 		else:
 			print('Formula IS NOT valid.')
